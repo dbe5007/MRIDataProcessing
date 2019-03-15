fbfd17e43c8ec12ed31570a2c899e1941571a6cb
Dan Elbich <dbe5007@psu.edu>
2019-03-15 13:19:10 -0400

    Renamed extension, updated README

--- dsiStudio/dsiStudioBatchRecodeDicom.sh
+++ dsiStudio/dsiStudioBatchRecodeDicom.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-# Recode DICOMS to squence name with .dcm extension
-# Daniel Elbich
-# The Pennsylvania State University
-# 7/13/17
-#
-#
-# Note!!!: Requires DSI Studio to run. Process will rename files of original data so
-# be sure to copy raw data to avoid overwriting.
-#
+
+####Notes & Comments####
+help() {
+echo ""
+echo "Recode DICOMS to squence name with .dcm extension"
+echo "Daniel Elbich"
+echo "The Pennsylvania State University"
+echo "Created: 7/13/17"
+echo ""
+echo ""
+echo " Batch rename DICOM files for set of subjects. Copys raw data folder and"
+echo " and renames copied files instead of altering original raw data folder."
+echo ""
+echo "Usage:"
+echo "sh dsiStudioBatchRecodeDicom.sh"
+echo ""
+echo ""
+exit 1
+}
+[ "$1" = "--help" ] && help
+
 
 #Subject List - list subject IDs with hard return after each
 #subjlist=(AA_123
@@ -30,7 +42,9 @@ do
 subjID=${subjlist[$i]}
 folderID=${folderlist[$i]}
 
-/path/to/install/dsi_studio --action=ren --source=/path/to/$subjID/$folderID
+cp /path/to/$subjID/$folderID /path/to/$subjID/$folderID"_dsi_renamed"
+
+/path/to/install/dsi_studio --action=ren --source=/path/to/$subjID/$folderID"_dsi_renamed"
 
 done
 
